2af1e16b93930701db3dfb7307fdc52f3764d68a
domoniquecodes <domonique.wilson89@icloud.com>
2024-04-17 14:14:14 -0400

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -3,5 +3,25 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://domonique-wilson-portfolio.netlify.app/about"
+          target="_blank"
+        >
+          Domonique Wilson
+        </a>{" "}
+        and is {""}
+        <a
+          href="https://github.com/domoniquecodes/final-react-weather-app"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
