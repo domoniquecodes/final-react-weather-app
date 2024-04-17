adf92d466c06426cdd7affc8ee3591b7c9ec968b
domoniquecodes <domonique.wilson89@icloud.com>
2024-04-17 14:46:25 -0400

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -5,23 +5,25 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://domonique-wilson-portfolio.netlify.app/about"
-          target="_blank"
-        >
-          Domonique Wilson
-        </a>{" "}
-        and is {""}
-        <a
-          href="https://github.com/domoniquecodes/final-react-weather-app"
-          target="_blank"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://domonique-wilson-portfolio.netlify.app/about"
+            target="_blank"
+          >
+            Domonique Wilson
+          </a>{" "}
+          and is {""}
+          <a
+            href="https://github.com/domoniquecodes/final-react-weather-app"
+            target="_blank"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
